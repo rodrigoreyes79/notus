e7a24ec4b7d5d0576387dbfa3dc97eb248b28ba1
Rodrigo Reyes <rodrigoreyes@nexa.uno>
2020-08-03 07:10:20 -0500

    Adding policies to AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,18 +2,26 @@
 
 namespace App\Providers;
 
+use App\ClassNote;
+use App\Policies\ClassNotePolicy;
+use App\Policies\SubjectPolicy;
+use App\Subject;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Silvanite\Brandenburg\Traits\ValidatesPermissions;
 
 class AuthServiceProvider extends ServiceProvider
 {
+
+    use ValidatesPermissions;
     /**
      * The policy mappings for the application.
      *
      * @var array
      */
     protected $policies = [
-        // 'App\Model' => 'App\Policies\ModelPolicy',
+        ClassNote::class => ClassNotePolicy::class,
+        Subject::class => SubjectPolicy::class
     ];
 
     /**
@@ -23,8 +31,26 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->registerPolicies();
+        collect([
+            'manageClassNotes',
+            'manageAllClassNotes',
+            'viewAllClassNotes',
+            'viewClassNotes',
+            'manageSubjects',
+            'manageAllSubjects',
+            'viewSubjects',
+            'viewAllSubjects',
+            'attachStudents'
+        ])->each(function ($permission) {
+            Gate::define($permission, function ($user) use ($permission) {
+                if ($this->nobodyHasAccess($permission)) {
+                    return true;
+                }
 
-        //
+                return $user->hasRoleWithPermission($permission);
+            });
+        });
+
+        $this->registerPolicies();
     }
 }
